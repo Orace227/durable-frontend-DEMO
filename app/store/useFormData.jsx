2f72f917c7e33b17caee7d07b26173cfc896da73
Orbit_Gamerz <gklibrary2020@gmail.com>
2024-07-02 16:29:09 +0530

    some static value removed

--- app/store/useFormData.jsx
+++ app/store/useFormData.jsx
@@ -7,13 +7,7 @@ const UserContext = createContext();
 
 // Create a context provider
 export const FormDataProvider = ({ children }) => {
-  const [formData, setFormData] = useState({
-    websiteType: "Business",
-    businessDescription:
-      "Explanation:\nloadingStates: Defines an array of loading steps, each with a text property indicating what each step represents.\nloading: Controls whether the loader is active (true) or not (false).\nduration: Optional prop to adjust how long each step remains visible before moving to the next step.\nloop: Optional prop to determine whether the loader should continuously loop through the steps (true) or stop after reaching the last step (false).",
-    businessName: "GlitchAstra",
-    logo: {},
-  });
+  const [formData, setFormData] = useState({});
   console.log(formData);
   const updateCustomer = (newUserData) => {
     setFormData((prevUser) => ({ ...prevUser, ...newUserData }));
